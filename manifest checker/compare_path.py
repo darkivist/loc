c038f6e73e3f36f53e0740dc3b9bd09b313bff40
Paul Kelly <kpaulj@protonmail.com>
2023-03-07 12:42:25 -0500

    Now ignores case.

--- manifest checker/compare_path.py
+++ manifest checker/compare_path.py
@@ -6,6 +6,7 @@ input1 = input("Please enter path to fileList.txt and press Enter: ")
 fileList = open(input1, "r")
 data = fileList.read()
 fileList_reformatted = data.replace('\n', '').split(",")
+fileList_caseless = map(str.lower,fileList_reformatted)
 #print(fileList_reformatted)
 
 input2 = input("Please enter path to content and press Enter: ")
@@ -13,11 +14,12 @@ input2 = input("Please enter path to content and press Enter: ")
 p = Path(input2).rglob('*')
 filePaths = [x for x in p if x.is_file()]
 filePaths_string = [str(x) for x in filePaths]
+filePaths_caseless = map(str.lower,filePaths_string)
 #print(filePaths_string)
 
 differences1 = []
-for element in fileList_reformatted:
-    if element not in filePaths_string:
+for element in fileList_caseless:
+    if element not in filePaths_caseless:
         differences1.append(element)
 
 print("The following files from the provided list were not found:",differences1)
@@ -28,8 +30,8 @@ with open("missing_files_report.csv","w",newline="\n") as f:
     wr.writerow(differences1)
 
 differences2 = []
-for element in filePaths_string:
-    if element not in fileList_reformatted:
+for element in filePaths_caseless:
+    if element not in fileList_caseless:
         differences2.append(element)
 
 print("The following unexpected files were found:",differences2)
